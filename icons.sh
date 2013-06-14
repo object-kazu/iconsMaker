57dfa5ab712fbbfb8cd15cf1dccfa2accf72d317
kazuyuki@mobile <kazuyukishimizu@me.com>
2013-06-14 14:07:36 +0900

    Default parameters are changed

--- icons.sh
+++ icons.sh
@@ -34,37 +34,13 @@
      ##### convert parameters ######
      readonly QUALITY=90
 
-     readonly RADIUS=1.5
-     readonly SIGMA=1.0
+     readonly RADIUS=0.5
+     readonly SIGMA=0.5
      readonly AMOUNT=1.7
      readonly THRESOLD=0.02
     
 
 
-######  Default color map ########
-
-export NONE=''
-export BLACK='\033[0;30m'
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export BROWN='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT_GREY='\033[0;37m'
-export DARK_GREY='\033[1;30m'
-export LIGHT_RED='\033[1;31m'
-export LIGHT_GREEN='\033[1;32m'
-export YELLOW='\033[1;33m'
-export LIGHT_BLUE='\033[1;34m'
-export LIGHT_PURPLE='\033[1;35m'
-export LIGHT_CYAN='\033[1;36m'
-export WHITE='\033[1;37m'
-export DEFAULT='\033[0;39m'
-export GREEN_U='\033[4;32m'
-export LIGHT_RED_U='\033[4;31m'
-export YELLOW_U='\033[4;33m'
-export BLUE_U='\033[4;34m'
 
 ######### functions ###############
 function display_help(){
